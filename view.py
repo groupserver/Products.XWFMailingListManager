510a3dfa11f74f795ecd627bfb526795c4733b46
Michael JasonSmith <mpj17@onlinegroups.net>
2006-10-29 23:29:43 +0000

    Committing the "GSPostContentProvider", a content provider for posts. The functionality of the single-page view of a post has not change, but the same code can now be used to display multiple posts. This should make displaying topics and recent posts a lot easier!
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFMailingListManager/trunk%401355

--- view.py
+++ view.py
@@ -2,7 +2,13 @@
 '''
 import sys, re, datetime, time, types
 import Products.Five, DateTime, Globals
-from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+#import Products.Five.browser.pagetemplatefile
+import zope.schema
+import zope.app.pagetemplate.viewpagetemplatefile
+zope.pagetemplate.pagetemplatefile
+import zope.interface, zope.component, zope.publisher.interfaces
+import zope.viewlet.interfaces, zope.contentprovider.interfaces 
+
 import DocumentTemplate
 
 import Products.GSContent
@@ -123,7 +129,124 @@ class GSPostView(Products.Five.BrowserView):
           assert self.topic
           retval = self.topic[-1]
           return retval
+          
+      def process_form(self):
+          pass
 
+# <zope-3 weirdness="high">
+
+class IGSPostContentProvider(zope.interface.Interface):
+      """The Groupserver Post Content Provider Interface
+      
+      This interface defines the fields that must be set up, normally using
+      TAL, before creating a "GSPostContentProvider" instance. See the
+      latter for an example."""
+      post = zope.schema.Field(title=u"Email Message Instance",
+                               description=u"The email instance to display",
+                               required=True, 
+                               readonly=False)
+zope.interface.directlyProvides(IGSPostContentProvider, 
+                                zope.contentprovider.interfaces.ITALNamespaceData)
+
+class GSPostContentProvider(object):
+      """GroupServer Post Content Provider: display a single post
+      
+      This content provider, which implements the "IGSPostContentProvider"
+      and "IContentProvider" interfaces, displays a single post. The post
+      is specified by setting the "post" variable to an instance of an 
+      email-object. The post-instance is examined, during the "update", to
+      determine additional information, which is passed to the email 
+      page-template during the "render" phase.
+      
+      EXAMPLE
+         <p tal:define="post python:view.get_email()"
+            tal:replace="structure provider:groupserver.Post">
+            The email message is rendered by the Post content provider,
+            not by this page.
+         </p>
+      """
+
+      zope.interface.implements(IGSPostContentProvider)
+      zope.component.adapts(zope.interface.Interface,
+                            zope.publisher.interfaces.browser.IDefaultBrowserLayer,
+                            zope.interface.Interface)
+      post = None
+      def __init__(self, context, request, view):
+          """Create a GSPostContentProvider instance.
+          
+          Like any other content-provider class, the context, request and
+          view are passed as arguments to "__init__". However, this is
+          normally done by TAL, rather than explicitly by the coders.
+          
+          SIDE EFFECTS
+            The following attributes are set.
+              * "self.__parent"     Set to "view".
+              * "self.__updated"    Set to "False"
+              * "self.context"      Set to "context"
+              * "self.request"      Set to "request"
+              * "self.pageTemplate" Set to the hard-coded page template 
+                                    object, which is used to render the 
+                                    post.
+              """
+          self.__parent__ = self.view = view
+          self.__updated = False
+      
+          self.context = context
+          self.request = request
+      
+          pageTemplateFileName = "browser/templates/email.pt"
+          VPTF = zope.pagetemplate.pagetemplatefile.PageTemplateFile
+          self.pageTemplate = VPTF(pageTemplateFileName)
+      
+      def update(self):
+          """Update the internal state of the post content-provider.
+          
+          This method can be considered the main "setter" for the 
+          content provider; for the most part, information about the post's 
+          author is set.
+          
+          SIDE EFFECTS
+            The following attributes are set.
+              * "self.__updated"     Set to "True"
+              * "self.authorId"      Set to the user-id of the post author.
+              * "self.authorName"    Set to the name of the post author.
+              * "self.authorExists"  Set to "True" if the author exists
+              * "self.authored"      Set to "True" if the current user 
+                                     authored the post.
+              * "self.authorImage"   Set to the URL of the author's image.
+          """
+          assert self.post
+          
+          self.__updated = True
+          
+          self.authorId = self.post.mailUserId;
+          self.authorName = self.get_author_realnames();
+          self.authorExists = self.author_exists();
+          self.authored = self.authorExists and self.user_authored();
+          self.authorImage = self.get_author_image()
+          assert self.__updated
+          
+      def render(self):
+          """Render the post
+          
+          The donkey-work of this method is done by "self.pageTemplate", 
+          which is set when the content-provider is created.
+          
+          RETURNS
+              An HTML-snippet that represents the post."""
+          if not self.__updated:
+              raise interfaces.UpdateNotCalled
+          return self.pageTemplate(authorId=self.authorId, 
+                                   authorName=self.authorName,
+                                   authorExists=self.authorExists,
+                                   authorImage=self.authorImage,
+                                   authored=self.authored,
+                                   post=self.post)
+
+      #########################################
+      # Non-standard methods below this point #
+      #########################################
+      
       def user_authored(self):
           '''Did the user write the email message?
           
@@ -136,14 +259,73 @@ class GSPostView(Products.Five.BrowserView):
               
           SIDE EFFECTS
               None.'''
-          assert self.email
-          assert self.user
-          retval = False
-          retval = self.context.user.getId() == self.email['mailUserId']
+          assert self.post
+          assert self.request
+          
+          user = self.request.AUTHENTICATED_USER
+          retval = user.getId() == self.post['mailUserId']
+          
           assert retval in (True, False)
           return retval
 
-      def process_form(self):
-          pass
+      def author_exists(self):
+          '''Does the author of the post exist?
+          
+          RETURNS
+             True if the author of the post exists on the system, False
+             otherwise.
+              
+          SIDE EFFECTS
+              None.'''
+      
+          assert self.post
+          retval = False
+          
+          authorId = self.post['mailUserId']
+          retval = self.context.Scripts.get.user_exists(authorId)
+          
+          assert retval in (True, False)
+          return retval
+      
+      def get_author_image(self):
+          """Get the URL for the image of the post's author.
+          
+          RETURNS
+             A string, representing the URL, if the author has an image,
+             "None" otherwise.
+             
+          SIDE EFFECTS
+             None.
+          """
+          assert self.post
 
+          retval = None          
+          if self.author_exists():
+              authorId = self.post['mailUserId']
+              retval = self.context.Scripts.get.user_image(authorId)
+          return retval
+           
+      def get_author_realnames(self):
+          """Get the names of the post's author.
+          
+          RETURNS
+              The name of the post's author. 
+          
+          SIDE EFFECTS
+             None.
+          """
+          assert self.post
+          
+          authorId = self.post['mailUserId']
+          retval = self.context.Scripts.get.user_realnames(authorId)
+          
+          return retval
+# State that the GSPostContentProvider is a Content Provider, and attach
+#     to "groupserver.Post".
+zope.component.provideAdapter(GSPostContentProvider, 
+                              provides=zope.contentprovider.interfaces.IContentProvider,
+                              name="groupserver.Post")
+
+# </zope-3 weirdness="high">
+          
 Globals.InitializeClass( GSPostView )
