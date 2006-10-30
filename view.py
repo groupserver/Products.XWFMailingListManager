1309a367008d4c87d2b3df0140ca0a7b40a21059
Michael JasonSmith <mpj17@onlinegroups.net>
2006-10-30 04:59:27 +0000

    The topic-view now is better, with the "previous" and "next" links working correctly. The current task is to get the "snipping" to work correctly, so the unncesssary quoting and signatures are removed. While the code to do this is present in the email content-provider, it has not been tested.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFMailingListManager/trunk%401359

--- view.py
+++ view.py
@@ -1,6 +1,6 @@
 '''GroupServer-Content View Class
 '''
-import sys, re, datetime, time, types
+import sys, re, datetime, time, types, string
 import Products.Five, DateTime, Globals
 #import Products.Five.browser.pagetemplatefile
 import zope.schema
@@ -11,11 +11,9 @@ import zope.viewlet.interfaces, zope.contentprovider.interfaces
 
 import DocumentTemplate
 
-import Products.GSContent
+import Products.GSContent, Products.XWFCore.XWFUtils
 
-class GSTopicView(Products.Five.BrowserView):
-      """View of a GroupServer Topic"""
-
+class GSBaseMessageView(Products.Five.BrowserView):
       def __init__(self, context, request):
           # Preconditions
           assert context
@@ -93,11 +91,72 @@ class GSTopicView(Products.Five.BrowserView):
           assert self.topic
           assert self.topic.append
           return self.topic
-
+      
+      def get_topic_name(self):
+          assert self.email
+          
+          retval = self.email['mailSubject']
+          
+          return retval
+          
       def process_form(self):
           pass
 
-class GSPostView(GSTopicView):
+class GSTopicView(GSBaseMessageView):
+      """View of a GroupServer Topic"""
+      def __init__(self, context, request):
+          # Preconditions
+          assert context
+          assert request
+           
+          GSBaseMessageView.__init__(self, context, request)
+
+          self.init_threads()
+
+      def init_threads(self):
+          assert self.topic
+          assert self.archive
+
+          self.threads  = self.archive.get_all_threads({}, 'mailDate', 'asc')
+          self.threadNames = map(lambda thread: thread[1][0]['mailSubject'], 
+                                 self.threads)
+          currThreadName = self.get_topic_name()
+          assert currThreadName in self.threadNames
+          self.currThreadIndex = self.threadNames.index(currThreadName)
+
+      def get_next_topic(self):
+          assert self.threads
+          
+          retval = None
+          
+          nextThreadIndex = self.currThreadIndex - 1
+          if nextThreadIndex >= 0:
+              ntID = self.threads[nextThreadIndex][1][0]['id']
+              ntName = self.threads[nextThreadIndex][1][0]['mailSubject']
+              retval = (ntID, ntName)
+          else:
+              retval = (None, None)
+      
+          assert len(retval) == 2
+          return retval
+          
+      def get_previous_topic(self):
+          assert self.threads
+          
+          retval = None
+
+          previousThreadIndex = self.currThreadIndex + 1
+          if previousThreadIndex < len(self.threads):
+              ptID = self.threads[previousThreadIndex][1][0]['id']
+              ptName = self.threads[previousThreadIndex][1][0]['mailSubject']
+              retval = (ptID, ptName)
+          else:
+              retval = (None, None)
+      
+          assert len(retval) == 2
+          return retval
+
+class GSPostView(GSBaseMessageView):
       """A view of a single post in a topic.
       
       A view of a single post shares much in common with a view of an 
@@ -110,7 +169,7 @@ class GSPostView(GSTopicView):
           assert context
           assert request
            
-          GSTopicView.__init__(self, context, request)
+          GSBaseMessageView.__init__(self, context, request)
 
       # Next and previous email messages
       def get_previous_email(self):
@@ -262,6 +321,172 @@ class GSPostContentProvider(object):
       #########################################
       # Non-standard methods below this point #
       #########################################
+      
+      def __markup_text(self, messageText):
+          """Mark up the plain text
+          
+          Used to mark up the email: the URLs are escaped, and "@"
+          characters are  replaced with "( at )". 
+          
+          ARGUMENTS
+              "messageText" The text to alter.
+               
+          RETURNS
+              A string containing the marked-up text.
+              
+          SIDE EFFECTS
+              None.
+
+          NOTE    
+              Originally found in XWFCore."""
+          import re, cgi
+          retval = ''
+          
+          text = cgi.escape(messageText)
+          text = re.sub('(?i)(http://|https://)(.+?)(\&lt;|\&gt;|\)|\]|\}|\"|\'|$|\s)',
+                 '<a href="\g<1>\g<2>">\g<1>\g<2></a>\g<3>',
+                 text)
+          text = text.replace('@', ' ( at ) ')
+      
+          retval =  text
+          
+          assert retval
+          assert retval.append
+          return retval
+      
+      def __wrap_message(self, messageText, width=79):
+          retval = ''
+          remaining = messageText
+          wrapped = []
+          
+          while len(remaining) > width:
+              cut = width
+              newline = find(remaining, '\n', 0, cut)
+          
+              if newline != -1:
+                  cut = newline
+              elif remaining[cut] != ' ':
+                  temp = rfind(remaining, ' ', 0, cut-1)
+                  if temp == -1:temp = find(remaining, ' ', cut-1, len(remaining))
+                  if temp == -1: temp = len(remaining)
+                  cut = temp
+              wrapped.append(remaining[:cut])
+              remaining = remaining[cut+1:]
+          
+          if remaining:
+              wrapped.append(remaining)
+          
+          retval = join(wrapped, '\n')
+          
+          assert retval
+          assert retval.append 
+          return retval
+
+      def __split_message(self, messageText, lines=0, 
+                          max_consecutive_comment=12, 
+                          max_consecutive_whitespace=3):
+                          
+          retval = ('', '')
+          slines = text.split('\n')
+
+          intro = []; body = []; i = 1;
+          bodystart = 0; consecutive_comment = 0; 
+          consecutive_whitespace = 0
+          
+          for line in slines:
+              if (line[:2] == '--' or line[:2] == '==' or line[:2] == '__' or
+                  line[:2] == '~~' or line [:3] == '- -'):
+                  bodystart = 1
+              
+              # if we've started on the body, just append to body
+              if bodystart: 
+                  body.append(line)
+              # count comments, but don't penalise top quoting as badly
+              elif consecutive_comment >= max_consecutive_comment and i > 25: 
+                  body.append(line)
+                  bodystart = 1
+              # if we've got less than 15 lines, just put it in the intro
+              elif (i <= 15):
+                  intro.append(line)
+              elif (len(line) > 3 and line[:4] != '&gt;'):
+                  intro.append(line)
+              elif consecutive_whitespace <= max_consecutive_whitespace:
+                  intro.append(line)
+              else:
+                  body.append(line)
+                  bodystart = 1
+              
+              if len(line) > 3 and (line[:4] == '&gt;' or line.lower().find('wrote:') != -1):
+                  consecutive_comment += 1
+              else:
+                  consecutive_comment = 0
+              
+              if len(line.strip()):
+                  consecutive_whitespace = 0
+              else:
+                  consecutive_whitespace += 1
+              
+              i += 1
+          
+          rintro = []; trim = 1
+          for line in intro[::-1]:
+              if len(intro) < 5:
+                  trim = 0
+              if len(line) > 3:
+                  ls = line[:4]
+              elif line.strip():
+                  ls = line.strip()[0]
+              else:
+                  ls = ''
+              
+              if trim and (ls == '&gt;' or ls == ''):
+                  body.insert(0, line)
+              elif trim and line.find('wrote:') > 2:
+                  body.insert(0, line)
+              elif trim and line.strip() and len(line.strip().split()) == 1:
+                  body.insert(0, line)
+              else:
+                  trim = 0
+                  rintro.insert(0, line)
+          
+          intro = '\n'.join(rintro)
+          body = '\n'.join(body)
+          retval = (intro, body)
+          
+          assert retval
+          assert len(retval) == 2
+          assert retval[0].append
+          assert retval[1].append
+          return retval
+          
+      def get_mail_body(self):
+          assert self.post
+          assert self.post['mailBody']
+
+          body = self.post['mailBody']
+          contentType = getattr(self.post, 'content-type', None)
+
+          ctct = Products.XWFCore.XWFUtils.convertTextUsingContentType
+          fullBody = ctct(body, contentType)  
+
+          messageLength = int(getattr(email, 'x-xwfnotification-message-length', 
+                              len(fullBody)))
+          
+          markedUpPost = self.__markup_text(fullBody[:messageLength]).strip()
+
+          assert retval
+          assert retval.append
+          return retval
+
+      def get_wrapped_email(self):
+          retval = self.__wrap_message(self.get_mail_body())
+          assert retval
+          assert retval.append
+          return retval
+      
+      def get_email_intro_and_remainder(self):
+          retval = self.__split_message(self.get_wrapped_email())
+          return retval
       
       def user_authored(self):
           """Did the user write the email message?
