f24d8d3284b59d9d30fc9954132c7db524319833
Michael JasonSmith <mpj17@onlinegroups.net>
2006-10-31 04:33:45 +0000

    Adding the disclosure button as a GIF, as I cannot be confident that others have climbed to the esoteric hights of Unicode.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFMailingListManager/trunk%401363

--- browser/javascript/showHideInlineElements.js
+++ browser/javascript/showHideInlineElements.js
@@ -3,7 +3,7 @@ function showHideInline(remainderId, discloseButtonId)
     var i=0;
     var remainderElement = document.getElementById(remainderId);
     var discloseElement = document.getElementById(discloseButtonId);
-    
+    var img = discloseElement.childNodes[1];
     const hiddenArrow = "\u25b6";
     const shownArrow = "\u25bc"
     
@@ -12,15 +12,19 @@ function showHideInline(remainderId, discloseButtonId)
         remainderElement.setAttribute('class', 'emailRemainderHidden');
         discloseElement.setAttribute('title', 
                                       "Show the email footer");
-        discloseElement.replaceChild(document.createTextNode(hiddenArrow),
-                                     discloseElement.childNodes[0]);
+        img.setAttribute("src",
+                         '/++resource++postImages/disclosure-arrow-hidden.gif');
+        //discloseElement.replaceChild(document.createTextNode(hiddenArrow),
+        //                             discloseElement.childNodes[0]);
     }
     else
     {
         remainderElement.setAttribute('class', 'emailRemainderShown');
         discloseElement.setAttribute('title', 
                                       "Hide the email footer");
-        discloseElement.replaceChild(document.createTextNode(shownArrow),
-                                     discloseElement.childNodes[0]);
+        img.setAttribute("src",
+                         '/++resource++postImages/disclosure-arrow-shown.gif');
+        //discloseElement.replaceChild(document.createTextNode(shownArrow),
+        //                             discloseElement.childNodes[0]);
     }
 }
