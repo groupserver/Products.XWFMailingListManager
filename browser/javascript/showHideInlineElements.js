ef6a33b13d3ae87fe7dd51ed504d2255f25a5594
Michael JasonSmith <mpj17@onlinegroups.net>
2007-02-09 01:28:37 +0000

    General clean up of the code, to get rid of 404s and other errors.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFMailingListManager/trunk%401543

--- browser/javascript/showHideInlineElements.js
+++ browser/javascript/showHideInlineElements.js
@@ -1,21 +1,17 @@
 function showHideInline(remainderId, discloseButtonId) 
 {
-    var i=0;
-    var remainderElement = document.getElementById(remainderId);
+    //const hiddenArrow = "\u25b6";
+    //const shownArrow = "\u25bc"
     var discloseElement = document.getElementById(discloseButtonId);
-    const hiddenArrow = "\u25b6";
-    const shownArrow = "\u25bc"
-    
+
     if (discloseElement.getAttribute('class') == 'shownArrow')
     { 
-        //remainderElement.setAttribute('class', 'emailRemainderHidden');
         discloseElement.setAttribute('title', "Show the footer");
         discloseElement.setAttribute('class', "hiddenArrow");
 
     }
     else
     {
-        //remainderElement.setAttribute('class', 'emailRemainderShown');
         discloseElement.setAttribute('title', "Hide the footer");
         discloseElement.setAttribute('class', "shownArrow");
     }
