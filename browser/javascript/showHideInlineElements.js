52465b26a2308bb31db17331d136c309bdcf0b77
Michael JasonSmith <mpj17@onlinegroups.net>
2007-02-15 22:51:06 +0000

    Changed the disclosure arrow from an image to a character. Now the arrow works in IE, but we *require* Unicode on the browser.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFMailingListManager/trunk%401603

--- browser/javascript/showHideInlineElements.js
+++ browser/javascript/showHideInlineElements.js
@@ -1,23 +1,17 @@
 function showHideInline(remainderId, discloseButtonId) 
 {
-    //const hiddenArrow = "\u25b6";
-    //const shownArrow = "\u25bc"
+    var hiddenArrow = "\u25b6";
+    var shownArrow = "\u25bc";
     var discloseElement = document.getElementById(discloseButtonId);
-
-    if (discloseElement.getAttribute('class') == 'shownArrow')
+    if (discloseElement.childNodes[0].data == shownArrow)
     { 
         discloseElement.setAttribute('title', "Show the footer");
-        discloseElement.setAttribute('class', "hiddenArrow");
-        //document.getElementById(remainderId).setAttribute('style',
-        //                                                  'display:none;');
-
+        discloseElement.childNodes[0].data = hiddenArrow;
     }
     else
     {
         discloseElement.setAttribute('title', "Hide the footer");
-        discloseElement.setAttribute('class', "shownArrow");
-        //document.getElementById(remainderId).setAttribute('style',
-        //                                                  'display:block;');
+        discloseElement.childNodes[0].data = shownArrow;
     }
     Effect.toggle(remainderId,'blind', {duration: 1, delay: 0});
 }
