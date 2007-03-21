1da372d4d475f3007a4a597d48b4572c2d03416a
Richard Waid <richard@iopen.net>
2007-03-21 02:24:50 +0000

    Update schema definition
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFMailingListManager/trunk%401789

--- sql/email.sql
+++ sql/email.sql
@@ -1,16 +1,28 @@
+SET client_encoding = 'UTF8';
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
 CREATE TABLE POST (
     POST_ID           TEXT                     PRIMARY KEY,
     TOPIC_ID          TEXT                     NOT NULL,
     GROUP_ID          TEXT                     NOT NULL,
     SITE_ID           TEXT                     NOT NULL,
     USER_ID           TEXT                     NOT NULL,
-    IN_REPLY_TO       TEXT                     NOT NULL DEFAULT '',
-    SUBJECT           TEXT                     NOT NULL DEFAULT '',
+    IN_REPLY_TO       TEXT                     NOT NULL DEFAULT ''::text,
+    SUBJECT           TEXT                     NOT NULL DEFAULT ''::text,
     DATE              TIMESTAMP WITH TIME ZONE NOT NULL,
-    BODY              TEXT                     NOT NULL DEFAULT '',
-    HTMLBODY          TEXT                     NOT NULL DEFAULT '',
+    BODY              TEXT                     NOT NULL DEFAULT ''::text,
+    HTMLBODY          TEXT                     NOT NULL DEFAULT ''::text,
     HEADER            TEXT                     NOT NULL,
     HAS_ATTACHMENTS   BOOLEAN		           NOT NULL
+);
+
+CREATE INDEX site_group_idx ON post USING btree (site_id, group_id);
+CREATE INDEX topic_idx ON post USING btree (topic_id);
+
+CREATE TABLE POST_TAG (
+	POST_ID			  TEXT                     NOT NULL REFERENCES POST (POST_ID),
+	TAG				  TEXT					   NOT NULL
 );
 
 CREATE TABLE TOPIC (
@@ -24,11 +36,15 @@ CREATE TABLE TOPIC (
     NUM_POSTS         INTEGER                  NOT NULL CHECK (NUM_POSTS > 0)
 );  
 
+CREATE INDEX group_id_site_id_idx ON topic USING btree (group_id, site_id);
+
 CREATE TABLE TOPIC_WORD_COUNT (
     TOPIC_ID          TEXT                     NOT NULL REFERENCES TOPIC (TOPIC_ID),
     WORD              TEXT                     NOT NULL,
     COUNT             INTEGER                  NOT NULL CHECK (COUNT > 0)
 );
+
+CREATE UNIQUE INDEX topic_word_pkey ON topic_word_count USING btree (topic_id, word);
 
 CREATE TABLE FILE (
     FILE_ID           TEXT                     NOT NULL,
@@ -38,9 +54,4 @@ CREATE TABLE FILE (
     DATE              TIMESTAMP WITH TIME ZONE NOT NULL,
     POST_ID           TEXT                     NOT NULL REFERENCES POST (POST_ID),
     TOPIC_ID          TEXT                     NOT NULL REFERENCES TOPIC (TOPIC_ID)
-);
-
-CREATE TABLE POST_TAG (
-	POST_ID			  TEXT                     NOT NULL REFERENCES POST (POST_ID),
-	TAG				  TEXT					   NOT NULL
 );
