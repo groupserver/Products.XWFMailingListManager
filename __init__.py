c84d4423585b997fe69c503334fff15d067d94ba
Richard Waid <richard@iopen.net>
2007-03-28 03:41:43 +0000

    Allow the query and datetime classes to be used from python scripts without fear of retribution from Zope security.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFMailingListManager/trunk%401889

--- __init__.py
+++ __init__.py
@@ -13,6 +13,17 @@ import postContentProvider, postMessageContentProvider
 import topicIndexContentProvider, topicSummaryContentProvider
 import stickyTopicToggleContentProvider
 
+from AccessControl import ModuleSecurityInfo
+from AccessControl import allow_class, allow_module, allow_type
+
+from queries import MessageQuery
+q_security = ModuleSecurityInfo('Products.XWFMailingListManager.queries')
+q_security.declarePublic('MessageQuery')
+allow_class(MessageQuery)
+
+from datetime import datetime
+allow_type(datetime)
+
 def initialize(context):
     # import lazily and defer initialization to the module
     XWFMailingListManager.initialize(context)
