db2baf2881b61fbc73db45f7ab6e7fd658da668f
Richard Waid <richard@iopen.net>
2007-04-11 03:20:52 +0000

    Make sure we work with lower case strings when looking for commands
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFMailingListManager/trunk%401964

--- XWFMailingList.py
+++ XWFMailingList.py
@@ -63,17 +63,17 @@ def check_for_commands(msg, commands):
     if not isinstance(commands, list) and not isinstance(commands, tuple):
         commands = (commands,)
         
-    cstring = filter_command_string(msg.subject)
+    cstring = filter_command_string(msg.subject).lower()
     for command in commands:
         # command must occur either at the start of string or after a space,
         # and at the end of a string, or be followed by a space
-        if re.search('( |^)%s( |$)' % command, cstring):
+        if re.search('( |^)%s( |$)' % command.lower(), cstring):
             return True
     
     # since that didn't work, try again with the first 100 chars of the body
-    cstring = filter_command_string(msg.body[:100])
+    cstring = filter_command_string(msg.body[:100]).lower()
     for command in commands:
-        if re.search('( |^)%s( |$)' % command, cstring):
+        if re.search('( |^)%s( |$)' % command.lower(), cstring):
             return True
         
     return False
