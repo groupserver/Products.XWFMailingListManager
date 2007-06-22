0fe7bfdf7993992c8b11aeb28fa0cd3bf24e1142
Michael JasonSmith <mpj17@onlinegroups.net>
2007-06-22 03:27:54 +0000

    Changes to n_dict.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFMailingListManager/trunk%402165

--- XWFMailingList.py
+++ XWFMailingList.py
@@ -697,7 +697,15 @@ class XWFMailingList(Folder):
                     'pin': pin(self.getValueFor('mailto'),
                                self.getValueFor('hashkey')),
                     'moderatedMessage': msg,
-                    'moderatedUser': moderatedUser}
+                    'moderatedUser': moderatedUser,
+                    'moderatedUserAddress': msg.sender,
+                    'groupName': self.title,
+                    'groupEmail': self.email,
+                    'subject': msg.subject,
+                    'mid': msg.post_id,
+                    'body': msg.body,
+                    'absolute_url': self.absolute_url(),
+                    'moderatedUserName': moderatedUser.getProperty('preferredName','')}
                   moderator.send_notification('mail_moderator', 'default',
                     n_dict=nDict)
 
