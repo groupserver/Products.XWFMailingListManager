7b648dc815e50e78c712a35e68949c8972e1438f
Richard Waid <richard@iopen.net>
2008-01-21 23:47:42 +0000

    Always thought that """ was the same as r" ... obviously I was wrong
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/XWFMailingListManager/trunk%403099

--- emailbody.py
+++ emailbody.py
@@ -1,6 +1,6 @@
 import re, cgi, textwrap
 
-email_matcher = re.compile("""\b([A-Z0-9._%+-]+)@([A-Z0-9.-]+\.[A-Z]{2,4})\b""",
+email_matcher = re.compile(r"![A-Z0-9._%+-]([A-Z0-9._%+-]+)@([A-Z0-9.-]+\.[A-Z]{2,4})\b",
                            re.I|re.M)
 
 def markup_text(messageText):
