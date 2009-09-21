701626dfbb68aa1d373049c2bbe27dbdc93a3e6c
Michael JasonSmith <mpj17@onlinegroups.net>
2009-09-21 10:38:37 +1000

    Adding the other parts of the messages.

--- postprivacy.py
+++ postprivacy.py
@@ -22,20 +22,24 @@ class GSPostPrivacyContentProvider(object):
         self.view = view
 
     def update(self):
-        siteInfo = createObject('groupserver.SiteInfo', 
-          self.context)
-        groupInfo = createObject('groupserver.GroupInfo', 
-          self.context)
-        userInfo = createObject('groupserver.LoggedInUser', 
-          self.context)
+        roles = rolesForPermissionOn('View', self.context)
+        anonView = 'Anonymous' in roles
           
-        anonView = 'Anonymous' in rolesForPermissionOn('View', 
-          self.context)
         self.visibility = anonView and u'public' or u'private'
-
-        self.webVisibility = u''
-        self.emailVisibility = u''
-
+        
+        if anonView:
+            self.webVisibility = u'<strong>Anyone</strong> &#8213; '\
+              u'including search engines and people who are not '\
+              u' logged in &#8213;'
+            self.emailVisibility = u'<strong>group</strong> members' 
+        elif 'DivisionMember' in roles:
+            self.webVisibility = u'<strong>Group</strong> members '\
+              u'and <strong>site</strong> members'
+            self.emailVisibility = u'<strong>group</strong> members' 
+        else:
+            self.webVisibility = u'<strong>Group</strong> members'
+            self.emailVisibility = u'' 
+        
         assert type(self.visibility) == unicode
         assert type(self.webVisibility) == unicode
         assert type(self.emailVisibility) == unicode
