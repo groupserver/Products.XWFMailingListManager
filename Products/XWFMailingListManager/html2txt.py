54a220abdeb5c8bb6afb20ef1f66965401b7d47e
Michael JasonSmith <mpj17@onlinegroups.net>
2011-07-06 10:09:53 +1000

    Adding some comments, and handling the situation where the link-text is the same as the href.

--- Products/XWFMailingListManager/html2txt.py
+++ Products/XWFMailingListManager/html2txt.py
@@ -17,6 +17,7 @@ class HTMLConverter(HTMLParser):
         HTMLParser.__init__(self)
         self.outText = u''
         self.lastHREF = []
+        self.lastData = u''
 
     def __unicode__(self):
         retval = self.dupeNewlineRE.sub('\n\n', self.outText)
@@ -26,19 +27,27 @@ class HTMLConverter(HTMLParser):
         return unicode(self).encode('ascii', 'ignore')
     
     def handle_starttag(self, tag, attrs):
+        # Remember the href attribute of the anchor, because it will
+        #   be displayed *after* the data. The attribute may not be
+        #   set because some crack smoking madman may have added anchor
+        #   *targets* to the message.
         if tag == 'a':
             attrsDict = dict(attrs)
             self.lastHREF.append(attrsDict.get('href', ''))
     
     def handle_endtag(self, tag):
+        # Display the value of the href attribute of the anchor, if set. 
+        #   Do not display the attribute value if it is the same as the
+        #   link-text.
         if tag == 'a' and self.lastHREF:
             href = self.lastHREF.pop()
-            if href:
+            if href and (href != self.lastData):
                 self.outText = self.outText + ' <%s>' % href
         
     def handle_data(self, data):
         d = data.strip()
         d = d and d or '\n'
+        self.lastData = d
         self.outText = self.outText + d
 
 def convert_to_txt(html):
