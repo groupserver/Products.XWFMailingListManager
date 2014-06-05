6071d15ee2403d15020bb702b95c9ed94b283987
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-04 22:44:40 -0700

    Moving the CREATE TABLE SQL to the newer products.

--- Products/XWFMailingListManager/sql/email.sql
+++ Products/XWFMailingListManager/sql/email.sql
@@ -2,40 +2,14 @@ SET CLIENT_ENCODING = 'UTF8';
 SET CHECK_FUNCTION_BODIES = FALSE;
 SET CLIENT_MIN_MESSAGES = WARNING;
 
-CREATE TABLE BOUNCE (
-    DATE              TIMESTAMP WITH TIME ZONE  NOT NULL,
-    USER_ID           TEXT                      NOT NULL,       
-    GROUP_ID          TEXT                      NOT NULL,
-    SITE_ID           TEXT                      NOT NULL,       
-    EMAIL             TEXT                      NOT NULL        
+-- A mapping from old post ID style to new post ID. this table is
+-- really only needed for backwards compatibility of GS earlier
+-- than 1.0
+CREATE TABLE post_id_map (
+    old_post_id  TEXT  NOT NULL,
+    new_post_id  TEXT  NOT NULL REFERENCES post(post_id)
 );
 
-CREATE TABLE POST_TAG (
-        POST_ID                   TEXT                     NOT NULL REFERENCES POST (POST_ID),
-        TAG                               TEXT                                     NOT NULL
-);
-
-CREATE TABLE group_digest (
-    site_id text not null,
-    group_id text not null,
-    sent_date timestamp with time zone not null
-);
-
-CREATE TABLE FILE (
-    FILE_ID           TEXT                     NOT NULL,
-    MIME_TYPE         TEXT                     NOT NULL,
-    FILE_NAME         TEXT                     NOT NULL,
-    FILE_SIZE         INTEGER                  NOT NULL,
-    DATE              TIMESTAMP WITH TIME ZONE NOT NULL,
-    POST_ID           TEXT                     NOT NULL REFERENCES POST (POST_ID),
-    TOPIC_ID          TEXT                     NOT NULL REFERENCES TOPIC (TOPIC_ID)
-);
-
--- A MAPPING FROM OLD POST ID STYLE TO NEW POST ID. THIS TABLE IS REALLY ONLY NEEDED
--- FOR BACKWARDS COMPATIBILITY OF GS EARLIER THAN 1.0
-CREATE TABLE POST_ID_MAP (
-    OLD_POST_ID           TEXT                                          NOT NULL,
-    NEW_POST_ID       TEXT                                              NOT NULL REFERENCES POST (POST_ID)
-);
-
-CREATE UNIQUE INDEX OLD_POST_ID_PKEY ON POST_ID_MAP USING BTREE (OLD_POST_ID);
+CREATE UNIQUE INDEX old_post_id_pkey 
+    ON post_id_map 
+    USING BTREE (old_post_id);
