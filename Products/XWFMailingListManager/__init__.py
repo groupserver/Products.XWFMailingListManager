a47ea8a2512385b17a41e2f531df88036d2707a3
Michael JasonSmith <mpj17@onlinegroups.net>
2015-01-29 14:21:50 -0800

    Woops

--- Products/XWFMailingListManager/__init__.py
+++ Products/XWFMailingListManager/__init__.py
@@ -7,14 +7,14 @@
 # to the head. Code which does not follow the rules will be rejected.
 #
 from __future__ import absolute_import, unicode_literals
-import XWFMailingListManager
-import XWFMailingList
-import XWFVirtualMailingListArchive2
+from . import XWFMailingListManager
+from . import XWFMailingList
+from . import XWFVirtualMailingListArchive2
 
 from AccessControl import ModuleSecurityInfo
 from AccessControl import allow_class, allow_type
 
-from queries import MessageQuery
+from .queries import MessageQuery
 
 q_security = ModuleSecurityInfo(b'Products.XWFMailingListManager.queries')
 q_security.declarePublic(b'MessageQuery')
